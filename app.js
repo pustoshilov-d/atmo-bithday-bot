a3bbd7aa2fcdae5408f0fb38debe7c379c05e8cd
Dmitry <DimkaPusha@gmail.com>
2020-03-06 02:36:28 +0300

    delete timeout

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const api = require('vk-easy');
 
-const {TOKEN, GROUP, CHAT, CHAT_TEST, PHOTO, TIME_OUT, TEST_FLAG} = require('./config');
+const {TOKEN, GROUP, CHAT, CHAT_TEST, PHOTO, TEST_FLAG} = require('./config');
 console.log('Hello');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -26,66 +26,60 @@ const keyboard = JSON.stringify({
     ]});
 
 
+api('messages.getConversationMembers', {
+    v:5.103,
+    access_token: TOKEN,
+    peer_id: CHAT,
+    fields: 'bdate',
+    group_id: GROUP,
+}).then(res => {
+    let Users = res.response.profiles;
 
-(function(){
-    api('messages.getConversationMembers', {
+    api('utils.getServerTime', {
         v:5.103,
         access_token: TOKEN,
-        peer_id: CHAT,
-        fields: 'bdate',
-        group_id: GROUP,
     }).then(res => {
-        let Users = res.response.profiles;
+        // console.log(res.response);
+        let date = new Date(res.response * 1000);
+        console.log('Сегодня ', date.getDay()+1, '.', date.getMonth()+1);
+        if (TEST_FLAG === '1') {
+            Users[0].bdate = (date.getDay()+1).toString()+'.'+ (date.getMonth()+1).toString();
+            console.log('Test: ', Users[0].bdate)} // удалить
+        let users_with_bday = [];
+        Users.forEach(user => {
+            if(user.bdate !== undefined){
 
-        api('utils.getServerTime', {
-            v:5.103,
-            access_token: TOKEN,
-        }).then(res => {
-            // console.log(res.response);
-            let date = new Date(res.response * 1000);
-            console.log('Сегодня ', date.getDay()+1, '.', date.getMonth()+1);
-            if (TEST_FLAG === '1') {
-                Users[0].bdate = (date.getDay()+1).toString()+'.'+ (date.getMonth()+1).toString();
-                console.log('Test: ', Users[0].bdate)} // удалить
-            let users_with_bday = [];
-            Users.forEach(user => {
-                if(user.bdate !== undefined){
-
-                    if (date.getDay()+1=== parseInt(user.bdate.split('.')[0]) && date.getMonth()+1 === parseInt(user.bdate.split('.')[1])) {
-                        users_with_bday.push('@id'+user.id+'('+user.first_name+')');
-                    }
-
-                    let text = '';
-
+                if (date.getDay()+1=== parseInt(user.bdate.split('.')[0]) && date.getMonth()+1 === parseInt(user.bdate.split('.')[1])) {
+                    users_with_bday.push('@id'+user.id+'('+user.first_name+')');
                 }
-            });
-            console.log(users_with_bday);
 
-            if (users_with_bday.length > 0){
-                let text = users_with_bday.join(', ').replace(/,\s([^,]+)$/, ' и $1') ;
-                text += messages[Math.floor((Math.random()*messages.length))];
-                if (users_with_bday.length > 1) {
-                    text = text.replace('тебя', 'вас').replace('тебе', 'вам');
-                }
-                console.log(text);
+                let text = '';
 
-                api('messages.send', {
-                    v:5.103,
-                    access_token: TOKEN,
-                    group_id: GROUP,
-                    peer_id: CHAT_TEST,
-                    random_id:  Math.floor(Math.random()*999999999),
-                    message: text,
-                    keyboard: keyboard,
-                    attachment: PHOTO,
-                }).then(console.log)
-            }
-            else {
-                console.log('Нет ДР сегодня')
             }
         });
+        console.log(users_with_bday);
+
+        if (users_with_bday.length > 0){
+            let text = users_with_bday.join(', ').replace(/,\s([^,]+)$/, ' и $1') ;
+            text += messages[Math.floor((Math.random()*messages.length))];
+            if (users_with_bday.length > 1) {
+                text = text.replace('тебя', 'вас').replace('тебе', 'вам');
+            }
+            console.log(text);
+
+            api('messages.send', {
+                v:5.103,
+                access_token: TOKEN,
+                group_id: GROUP,
+                peer_id: CHAT_TEST,
+                random_id:  Math.floor(Math.random()*999999999),
+                message: text,
+                keyboard: keyboard,
+                attachment: PHOTO,
+            }).then(console.log)
+        }
+        else {
+            console.log('Нет ДР сегодня')
+        }
     });
-
-setTimeout(arguments.callee, TIME_OUT);
-
-})();
+});
