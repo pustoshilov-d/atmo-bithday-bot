1548c1aca36e5484f6466baf2ca8c484bbe6be4a
Dmitry <DimkaPusha@gmail.com>
2020-03-06 18:31:02 +0300

    add database of Days

--- db/checkDaysDB.js
+++ db/checkDaysDB.js
@@ -3,7 +3,7 @@ const createPool = require('./dbConnection.js');
 module.exports = async (dateStr) => {
     var res = undefined;
     const pool = await createPool();
-    const sql = `SELECT * FROM days WHERE day = ${dateStr}`;
+    const sql = `SELECT * FROM days WHERE day = '${dateStr}'`;
     try {
         res = await pool.query(sql);
     } catch (err) {
