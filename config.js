75b53c75f82b2c6723b7c628881abea7b78dbe3c
DimkaPusha@gmail.com <Dimkach3>
2020-06-27 01:07:06 +0300

    add button for congr

--- config.js
+++ config.js
@@ -9,10 +9,5 @@ module.exports = {
     TIME:process.env.TIME,
     EXTERNAL_DB_FLAG:process.env.EXTERNAL_DB_FLAG,
 
-    HOST: process.env.HOST,
-    PORT_DB: process.env.PORT_DB,
-    DATABASE: process.env.DATABASE,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL
 };
