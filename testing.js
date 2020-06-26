4571d4bc0758ae99ac02479d4f77e96187fbd468
DimkaPusha@gmail.com <Dimkach3>
2020-06-27 00:18:38 +0300

    add button for congr

--- testing.js
+++ testing.js
@@ -4,22 +4,32 @@ const {Pool} = require('pg');
 main();
 
 async function main() {
-
-    console.log('sql');
-    let pool = new Pool({
-        connectionString: DATABASE_URL,
-        ssl: {
-            sslmode: 'require',
-            rejectUnauthorized: false
+    try {
+        console.log('sql');
+        let pool = new Pool({
+            connectionString: DATABASE_URL,
+            ssl: {
+                sslmode: 'require',
+                rejectUnauthorized: false
+            }
+        })
+        function sayHi() {
+            alert('Привет');
         }
-    })
 
-    pool.connect().then((res, err)=>{
-        console.log("Результат коннект", res, err)
-    });
+        setTimeout(sayHi, 1000);
 
-    pool.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-        console.log(err ? err.stack : res.rows[0].message) // Hello World!
-        pool.end()
-    })
+
+        pool.connect().then((res, err) => {
+            console.log("Результат коннект", res, err)
+        });
+
+        pool.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+            console.log(err ? err.stack : res.rows[0].message) // Hello World!
+            pool.end()
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
 }
