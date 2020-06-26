3685c3584341764b2843acf82a15780bfe906f5f
DimkaPusha@gmail.com <Dimkach3>
2020-06-27 00:33:32 +0300

    add button for congr

--- testing.js
+++ testing.js
@@ -9,32 +9,13 @@ async function main() {
         let pool = new Pool({
             connectionString: DATABASE_URL,
             ssl: {
-                sslmode: 'require',
                 rejectUnauthorized: false
             }
         })
-        function sayHi() {
-            return console.log('Привет');
-        }
 
 
-        var promise = new Promise((resolve, reject) => {
 
-            setTimeout(() => {
-                reject(new Error("время вышло!"));
-            }, 1000);
-
-        });
-        promise
-            .then(
-                result => console.log("Fulfilled: " + result),
-                error => console.log("Rejected: " + error.message) // Rejected: время вышло!
-            );
-
-
-        pool.connect().then((res, err) => {
-            console.log("Результат коннект", res, err)
-        });
+        pool.connect()
 
         pool.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
             console.log(err ? err.stack : res.rows[0].message) // Hello World!
