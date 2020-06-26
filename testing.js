36a5fe1806c52722c8340d09311c71b6246a6c8f
DimkaPusha@gmail.com <Dimkach3>
2020-06-27 01:14:53 +0300

    add button for congr

--- testing.js
+++ testing.js
@@ -1,5 +1,7 @@
 const {Pool, Client} = require('pg');
 
+console.log('start');
+
 let pool = new Client({
     connectionString: 'postgres://xuxjeghczikrhq:95c79dffb6232fcb4d10adfe999a499b3057a60cd651cce3e69005314d1a9f41@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/d3gl4rnjqtbtfs',
     ssl: {
@@ -7,11 +9,13 @@ let pool = new Client({
         rejectUnauthorized: false
     }
 })
+console.log('create');
 
 pool.connect();
+console.log('connect');
 
 pool.query('SELECT \'message\'', (err, res) => {
-    console.log("connect")
+    console.log("end")
     console.log(err ? err : res.rows[0]) // Hello World!
     pool.end()
 })
