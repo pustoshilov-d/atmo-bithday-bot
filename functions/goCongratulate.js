d1389828ea004f586153c174f1ca0fa7c59320e2
DimkaPusha@gmail.com <Dimkach3>
2020-06-26 22:59:46 +0300

    add button for congr

--- functions/goCongratulate.js
+++ functions/goCongratulate.js
@@ -2,11 +2,18 @@ const {TIME} = require('../config');
 const checkDaysDB = require('../db/checkDaysDB');
 
 module.exports = async (hour, dateStr) =>{
-    console.log('Текущий час',hour);
-    let result;
-    if (hour < TIME) {result = 1}
-    else if(await checkDaysDB(dateStr)){result = 0}
-    else {result = 2}
-    console.log(result)
-    return result
+    try{
+        console.log('Текущий час',hour);
+        console.log(TIME);
+        console.log(await checkDaysDB(dateStr))
+        let result;
+        if (hour < TIME) {result = 1}
+        else if(await checkDaysDB(dateStr)){result = 0}
+        else {result = 2}
+        console.log(result)
+        return result
+    }
+    catch (e) {
+        console.log("Ошибка в goCong", e)
+    }
 };
