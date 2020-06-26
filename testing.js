1fce6e89c77f84a41c651bf762ac601f26e1d1d7
DimkaPusha@gmail.com <Dimkach3>
2020-06-27 01:04:46 +0300

    add button for congr

--- testing.js
+++ testing.js
@@ -1,17 +1,17 @@
-const {DATABASE_URL} = require('./config');
 const {Pool, Client} = require('pg');
-
-
-
 console.log('sql', DATABASE_URL);
 let pool = new Client({
     connectionString: 'postgres://xuxjeghczikrhq:95c79dffb6232fcb4d10adfe999a499b3057a60cd651cce3e69005314d1a9f41@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/d3gl4rnjqtbtfs',
+    ssl: {
+        sslmode: 'require',
+        rejectUnauthorized: false
+    }
 })
 
 pool.connect();
 
-pool.query('SELECT * from chats', (err, res) => {
+pool.query('SELECT \'message\'', (err, res) => {
     console.log("connect")
-    console.log(err ? err : res) // Hello World!
+    console.log(err ? err : res.rows[0]) // Hello World!
     pool.end()
 })
