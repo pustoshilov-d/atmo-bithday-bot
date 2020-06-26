34700e2b6e9c99bd942d8a40f67af33dddd31ce8
DimkaPusha@gmail.com <Dimkach3>
2020-06-26 23:41:02 +0300

    add button for congr

---
+++ testing.js
@@ -0,0 +1,25 @@
+const {DATABASE_URL} = require('../config.js');
+const {Pool} = require('pg');
+
+main();
+
+async function main() {
+
+    console.log('sql');
+    let pool = new Pool({
+        connectionString: DATABASE_URL,
+        ssl: {
+            sslmode: 'require',
+            rejectUnauthorized: false
+        }
+    })
+
+    console.log('sql2', pool);
+
+    await pool.connect();
+    console.log('sql2', pool);
+    const sql = `SELECT * FROM days`;
+    console.log(sql)
+    let res = await pool.query(sql);
+    console.log('Результат CheckDaysDB', res)
+}
